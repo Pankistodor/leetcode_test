e5659d66c132451bf6304f5a772e7cd0571ab2e1
Павлов Андрей <pankistodor@gmail.com>
2021-04-08 16:40:22 +0300

    08.04.2021

---
+++ 21. Merge Two Sorted Lists_no.py
@@ -0,0 +1,11 @@
+"""
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists
+"""
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        pass
+
+if __name__ == "__main__":
+    sol = Solution()
+    # tests
